bbcb6980d1f9c4bd84226afa7af4af42933ebad1
王佳裕 <jiayu.wang@iyouke.com>
2023-06-12 16:12:02 +0800

    init

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function(options) {
+			console.log('App Launch', JSON.stringify(options))
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ pages/test/test.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		test
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(options) {
+			console.log('test onload', JSON.stringify(options))
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
